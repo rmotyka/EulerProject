e2629fa4d690e8924ab2fcce1a823941fde68adb
r.motyka <r.motyka@nexidigital.eu>
2024-07-12 15:39:09 +0200

    Problem15 with dynamic

--- EulerPoject/Problem15.cs
+++ EulerPoject/Problem15.cs
@@ -17,72 +17,33 @@ namespace EulerPoject
         {
             return $"({X}, {Y})";
         }
-
-        public List<Point> GetNextPoints(Point from, Point maxPoint)
-        {
-            var output = new List<Point>();
-            if (from.X < maxPoint.X)
-            {
-                output.Add(new Point { X = X + 1, Y = Y });
-            }
-
-            if (from.Y < maxPoint.Y)
-            {
-                output.Add(new Point { X = X, Y = Y + 1 });
-            }
-
-            return output;
-        }
-
-        public bool IsPreviousTo(Point to)
-        {
-            bool sameRow = X == to.X && to.Y - Y == 1;
-            bool sameCol = Y == to.Y && to.X - X == 1;
-
-            return sameCol || sameRow;
-        }
     }
 
     internal class Problem15
     {
         public void Calculate()
         {
-            /*            var n = 20;
-                        long suma = 0;
-                        for (int i = 1; i <= n; i++)
-                        {
-                            suma += (long)Math.Pow(2, i);
-                        }
+            const int maxDim = 21;
+            int lastIndex = maxDim - 1;
 
-                        Console.WriteLine($"suma {suma}");*/
+            var nodes = new ulong[maxDim, maxDim];
 
-            Point startPoint = new Point { X = 0, Y = 0 };
-            Point maxPoint = new Point { X = 20, Y = 20 };
-
-            var numberOfRoutes = NumberOfRoutes(startPoint, maxPoint);
-
-            Console.WriteLine($"Number of rutes {numberOfRoutes}");
-        }
-
-        private ulong NumberOfRoutes(Point from, Point maxPoint)
-        {
-            Console.SetCursorPosition(from.X, from.Y);
-            Console.Write(".");
-            if (from.IsPreviousTo(maxPoint))
+            for (int row = lastIndex; row >= 0; row--)
             {
-                return 1;
-
-            }
-            var nextPoints = from.GetNextPoints(from, maxPoint);
-            ulong sum = 0; 
-            foreach (var nextPoint in nextPoints)
-            {
-                var nuberOfRoutes = NumberOfRoutes(nextPoint, maxPoint);
-                sum += nuberOfRoutes;
+                for (int col = lastIndex; col >= 0; col--)
+                {
+                    if (col == lastIndex || row == lastIndex)
+                    {
+                        nodes[row, col] = 1;
+                    }
+                    else
+                    {
+                        nodes[row, col] = nodes[row + 1, col] + nodes[row, col + 1];
+                    }
+                }
             }
 
-            return sum;
+            Console.WriteLine(nodes[0, 0]);
         }
-
     }
 }
