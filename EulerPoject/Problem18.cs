873b27c4104c27a7b048fd658d300811ba506c5a
r.motyka <r.motyka@nexidigital.eu>
2024-07-18 15:59:21 +0200

    improvements

--- EulerPoject/Problem18.cs
+++ EulerPoject/Problem18.cs
@@ -7,10 +7,10 @@
         {
             long[,] input = 
             {
-                {3, 0 ,0, 0 },
-                {7, 4, 0, 0 },
-                {2, 4, 6, 0 },
-                {8, 5, 9, 3 },
+                {3, 0 ,0, 0 }, // 0
+                {7, 4, 0, 0 }, // 1
+                {2, 4, 6, 0 }, // 2
+                {8, 5, 9, 3 }, // 3
             };
 
             int lastColumnIndex = input.GetUpperBound(0);
@@ -18,7 +18,10 @@
 
             for (int row = lastRowIndex - 1; row >= 0; row--)
             {
-                for (int col = 0; col < lastColumnIndex; col++)
+                // data is triangle
+                var lastColumntIndexWithData = row;
+
+                for (int col = 0; col <= lastColumntIndexWithData; col++)
                 {
                     var currentValue = input[row, col];
                     var previous1 = input[row + 1, col];
@@ -34,6 +37,8 @@
                     input[row, col] = currentValue;
                 }
             }
+
+            Console.WriteLine(input[0,0]);
         }
     }
 }
