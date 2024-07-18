428d053e75529f067b9863da203ea8fb4385a5e6
r.motyka <r.motyka@nexidigital.eu>
2024-07-18 15:50:24 +0200

    wip problem 18

---
+++ EulerPoject/Problem18.cs
@@ -0,0 +1,39 @@
+﻿namespace EulerPoject
+{
+
+    public class Problem18
+    {
+        public void Calculate()
+        {
+            long[,] input = 
+            {
+                {3, 0 ,0, 0 },
+                {7, 4, 0, 0 },
+                {2, 4, 6, 0 },
+                {8, 5, 9, 3 },
+            };
+
+            int lastColumnIndex = input.GetUpperBound(0);
+            int lastRowIndex = input.GetUpperBound(1);
+
+            for (int row = lastRowIndex - 1; row >= 0; row--)
+            {
+                for (int col = 0; col < lastColumnIndex; col++)
+                {
+                    var currentValue = input[row, col];
+                    var previous1 = input[row + 1, col];
+                    var previous2 = input[row + 1, col + 1];
+                    if (previous1 > previous2)
+                    {
+                        currentValue += previous1;
+                    }
+                    else
+                    {
+                        currentValue += previous2;
+                    }
+                    input[row, col] = currentValue;
+                }
+            }
+        }
+    }
+}
--- EulerPoject/Program.cs
+++ EulerPoject/Program.cs
@@ -3,5 +3,5 @@
 //var problem14 = new Problem14();
 //problem14.Calculate();
 
-var problem15 = new Problem15();
-problem15.Calculate();
+var problem18 = new Problem18();
+problem18.Calculate();
