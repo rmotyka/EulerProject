d867a37dc9f53409274aea9602125f9d762e44ed
r.motyka <r.motyka@nexidigital.eu>
2024-07-19 12:41:34 +0200

    Problem 19

--- EulerPoject/Problem19.cs
+++ EulerPoject/Problem19.cs
@@ -16,16 +16,78 @@ namespace EulerPoject
             var currentWeekday = 1; // 1 .. 7
 
             var firstSundaysCount = 0;
-            var leapYear = GetLeapYears(1900, 2000);
+            var leapYears = GetLeapYears(1900, 2000);
 
             while(true)
             {
-                var newDayOfMonth = currentDayOfMonth + 1;
-                
+                //var currentDate = $"{currentYear}-{currentMonth}-{currentDayOfMonth} = {currentWeekday}";
+                //Console.WriteLine(currentDate);
 
+                if (currentYear >= 1901 && currentDayOfMonth == 1 && currentWeekday == 7)
+                {
+                    firstSundaysCount++;
+                }
+
+                var monthLastDay = GetMonthLastDay(leapYears, currentYear, currentMonth);
+
+                // calculate next day
+                if (currentDayOfMonth == monthLastDay)
+                {
+                    // next month or year
+                    if (currentMonth == 12)
+                    {
+                        currentMonth = 1;
+                        currentYear++;
+                    }
+                    else
+                    {
+                        currentMonth++;
+                    }
+
+                    currentDayOfMonth = 1;
+                }
+                else
+                {
+                    currentDayOfMonth++;
+                }
+
+                if (currentWeekday == 7)
+                {
+                    currentWeekday = 1;
+                }
+                else
+                {
+                    currentWeekday++;
+                }
+
+                if (currentYear == 2000 && currentMonth == 12 && currentDayOfMonth == 31)
+                {
+                    break;
+                }
             }
 
             Console.WriteLine(firstSundaysCount);
+        }
+
+        private int GetMonthLastDay(List<int> leapYears, int currentYear, int currentMonth)
+        {
+            var isLeapYear = leapYears.Contains(currentYear);
+            return currentMonth switch
+            {
+                1 => 31,
+                2 => isLeapYear ? 29 : 28,
+                3 => 31,
+                4 => 30,
+                5 => 31,
+                6 => 30,
+                7 => 31,
+                8 => 31,
+                9 => 30,
+                10 => 31,
+                11 => 30,
+                12 => 31,
+                _ => throw new ArgumentException(),
+            };
         }
 
         private List<int> GetLeapYears(int startingYear, int endingYear)
