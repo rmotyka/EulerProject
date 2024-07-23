afc87da16f8f419b1f65944f1e249c3369702c46
r.motyka <r.motyka@nexidigital.eu>
2024-07-23 10:39:57 +0200

    read names

--- EulerPoject/Problem22.cs
+++ EulerPoject/Problem22.cs
@@ -10,7 +10,36 @@ namespace EulerPoject
     {
         public void Calculate()
         {
+            var names = ReadNamesFile();
 
+        }
+
+        private List<string> ReadNamesFile()
+        {
+            var names = new List<string>(60_000);
+
+            using StreamReader sr = new StreamReader("Resources\\Problem22\\0022_names.txt");
+            int character;
+            var sb = new StringBuilder();
+            while((character = sr.Read()) != -1)
+            {
+                char c = (char)character;
+                if (c == '"')
+                {
+                    continue;
+                }
+
+                if (c == ',')
+                {
+                    names.Add(sb.ToString());
+                    sb.Clear();
+                    continue;
+                }
+
+                sb.Append(c);
+            }
+
+            return names;
         }
     }
 }
